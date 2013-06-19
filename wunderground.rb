e1e10520ac4a146ac787e2b690e1b47c82570cf2
ZPH <Zander@civet.ws>
2013-06-19 18:31:49 -0400

    Round 1 of Attempts, unsanitized

--- wunderground.rb
+++ wunderground.rb
@@ -1,12 +1,17 @@
 require 'open-uri'
 require 'json'
+require 'yaml'
+require 'net/http'
+require 'pry'
 
-api_key = '' # required by producing your own API key from http://www.wunderground.com/weather/api/ 
-
-open('http://api.wunderground.com/api/api_key/geolookup/conditions/q/IA/Cedar_Rapids.json') do |f|
-  json_string = f.read
-  parsed_json = JSON.parse(json_string)
-  location = parsed_json['location']['city']
-  temp_f = parsed_json['current_observation']['temp_f']
-  print "Current temperature in #{location} is: #{temp_f}\n"
-end
+api_key = YAML.load_file(File.expand_path "~/.wunderground")['key']
+url = "http://api.wunderground.com/api/#{ api_key }/geolookup/conditions/q/IA/Cedar_Rapids.json"
+#
+File.write("curl_raw.txt", `curl -ivs --raw #{url}`)
+# open(url) do |f|
+#   json_string = f.read
+#   parsed_json = JSON.parse(json_string)
+#   location = parsed_json['location']['city']
+#   temp_f = parsed_json['current_observation']['temp_f']
+#   print "Current temperature in #{location} is: #{temp_f}\n"
+# end
